8b15e4a2dc024cc4cc06df353e5d03a1c9850801
Shenwy <swydesky@hotmail.com>
2019-10-14 16:01:56 +0800

    test webhook

--- src/main/java/com/tf/demo/DemoController.java
+++ src/main/java/com/tf/demo/DemoController.java
@@ -2,17 +2,23 @@ package com.tf.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DemoController {
 	
 	private static Logger log = LoggerFactory.getLogger(DemoController.class);
 	
 	@RequestMapping("/hello")
-	public String testDemo2() {
+	public String testDemo() {
 		log.info("test success");
 		return "hello";
+	}
+	
+	@RequestMapping("/test")
+	public String test() {
+		log.info("test success");
+		return "success";
 	}
 }
