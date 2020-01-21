38dd3f2196d5b35ecdfc30c246dfff540ab7f585
Shenwy <swydesky@hotmail.com>
2020-01-21 15:07:34 +0800

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 
-#!/usr/bin/env bash
+#!/usr/bin/expect
 
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
@@ -20,3 +20,13 @@ echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
+
+spawn ssh root@172.16.50.216
+
+expect "*password:" {send "root.123\r"}
+expect "*'yes' or 'no':" {send "yes\r"}
+expect "*]#" {
+  send "touch 111.txt\r"
+}
+
+interact
