a64a77bafc4205695cd0060c304ae67970014460
Shenwy <swydesky@hotmail.com>
2020-01-22 15:12:27 +0800

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,32 +1,15 @@
 
 #!/usr/bin/expect
-
-echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
-set -x
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
-set +x
-
-echo 'The following complex command extracts the value of the <name/> element'
-echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 set +x
 
-echo 'The following complex command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
 
 spawn ssh root@172.16.50.216
-
 expect "*password:" {send "root.123\r"}
 expect "*'yes' or 'no':" {send "yes\r"}
-expect "*]#" {
-  send "touch 111.txt\r"
-}
-
+expect "*]#" {send "touch 111.txt\r"}
 interact
